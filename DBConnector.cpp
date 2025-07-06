3b689ae3fa6744013bd9b79933bc29b04d0f2f8b
秦政 <7525396+qin_z@user.noreply.gitee.com>
2025-07-06 13:52:17 +0800

    更新数据库操作

--- DBConnector.cpp
+++ DBConnector.cpp
@@ -40,15 +40,13 @@ std::vector<std::vector<std::string>> DBConnector::query(const std::string &sql)
 
     MYSQL_RES *res = mysql_store_result(conn);
     if (!res)
-    {
-        return results; // 非查询语句
-    }
+        return results;
 
     MYSQL_ROW row;
     unsigned int num_fields = mysql_num_fields(res);
-
-    // 获取列名
     MYSQL_FIELD *fields = mysql_fetch_fields(res);
+
+    // 添加列名
     std::vector<std::string> column_names;
     for (unsigned int i = 0; i < num_fields; ++i)
     {
@@ -56,7 +54,7 @@ std::vector<std::vector<std::string>> DBConnector::query(const std::string &sql)
     }
     results.push_back(column_names);
 
-    // 获取行数据
+    // 添加行数据
     while ((row = mysql_fetch_row(res)))
     {
         std::vector<std::string> row_data;
@@ -74,4 +72,49 @@ std::vector<std::vector<std::string>> DBConnector::query(const std::string &sql)
 bool DBConnector::update(const std::string &sql)
 {
     return mysql_query(conn, sql.c_str()) == 0;
+}
+
+// 创建用户
+bool DBConnector::createUser(const std::string &studentID, const std::string &name, const std::string &gender, const std::string &major, int enrollmentYear, const std::string &contactInfo, const std::string &username, const std::string &password, bool isAdmin)
+{
+    std::string sql = "INSERT INTO users (studentID, name, gender, major, enrollmentYear, contactInfo, username, password, isAdmin) VALUES ('" +
+                      studentID + "', '" + name + "', '" + gender + "', '" + major + "', " + std::to_string(enrollmentYear) + ", '" +
+                      contactInfo + "', '" + username + "', '" + password + "', " + (isAdmin ? "1" : "0") + ")";
+    return update(sql);
+}
+
+// 按用户名查询用户
+std::vector<std::vector<std::string>> DBConnector::getUserByUsername(const std::string &username)
+{
+    std::string sql = "SELECT * FROM users WHERE username = '" + username + "'";
+    return query(sql);
+}
+
+// 查询所有用户
+std::vector<std::vector<std::string>> DBConnector::getAllUsers()
+{
+    return query("SELECT * FROM users");
+}
+
+// 更新用户信息（不含密码）
+bool DBConnector::updateUser(const std::string &studentID, const std::string &name, const std::string &gender, const std::string &major, int enrollmentYear, const std::string &contactInfo, const std::string &username, bool isAdmin)
+{
+    std::string sql = "UPDATE users SET name = '" + name + "', gender = '" + gender + "', major = '" + major + "', enrollmentYear = " +
+                      std::to_string(enrollmentYear) + ", contactInfo = '" + contactInfo + "', username = '" + username + "', isAdmin = " +
+                      (isAdmin ? "1" : "0") + " WHERE studentID = '" + studentID + "'";
+    return update(sql);
+}
+
+// 更新密码
+bool DBConnector::updateUserPassword(const std::string &username, const std::string &newPassword)
+{
+    std::string sql = "UPDATE users SET password = '" + newPassword + "' WHERE username = '" + username + "'";
+    return update(sql);
+}
+
+// 删除用户
+bool DBConnector::deleteUser(const std::string &studentID)
+{
+    std::string sql = "DELETE FROM users WHERE studentID = '" + studentID + "'";
+    return update(sql);
 }
--- DBConnector.h
+++ DBConnector.h
@@ -1,9 +1,9 @@
 #ifndef DBCONNECTOR_H
 #define DBCONNECTOR_H
 
-#include <mysql.h>
 #include <string>
 #include <vector>
+#include <mysql.h>
 
 class DBConnector
 {
@@ -14,7 +14,17 @@ private:
 
 public:
     ~DBConnector();
-    static DBConnector *getInstance(const std::string &host = "localhost", const std::string &user = "root", const std::string &password = "qz284781", const std::string &dbname = "grad_student_management", unsigned int port = 3306);
+    static DBConnector *getInstance(const std::string &host = "localhost", const std::string &user = "root", const std::string &password = "your_password", const std::string &dbname = "grad_student_management", unsigned int port = 3306);
+
+    // Users表CRUD操作
+    bool createUser(const std::string &studentID, const std::string &name, const std::string &gender, const std::string &major, int enrollmentYear, const std::string &contactInfo, const std::string &username, const std::string &password, bool isAdmin = false);
+    std::vector<std::vector<std::string>> getUserByUsername(const std::string &username);
+    std::vector<std::vector<std::string>> getAllUsers();
+    bool updateUser(const std::string &studentID, const std::string &name, const std::string &gender, const std::string &major, int enrollmentYear, const std::string &contactInfo, const std::string &username, bool isAdmin);
+    bool updateUserPassword(const std::string &username, const std::string &newPassword);
+    bool deleteUser(const std::string &studentID);
+
+private:
     std::vector<std::vector<std::string>> query(const std::string &sql);
     bool update(const std::string &sql);
 };
